bd3a470b8e4cd2fa64ddf953198d6387a32c7543
Fabien Tarrade <fabien.tarrade@gmail.com>
2020-06-17 13:36:44 +0200

    adding base image

--- docker/base_container/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-printenv
-conda info --envs
-conda info
-. "${DL_ANACONDA_HOME}/etc/profile.d/conda.sh"
-conda activate env_multilingual_class
-conda info -e
-exec "$@"
--- docker/test/copy_files.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp -r proj_multilingual_text_classification/src proj_multilingual_text_classification/docker/$DOCKERFILE/.
-cp proj_multilingual_text_classification/env/environment.yaml proj_multilingual_text_classification/docker/$DOCKERFILE/.
