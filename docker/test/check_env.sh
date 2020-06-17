587152b7fa7402097b49c87b37cb51c2443bf8ba
Fabien Tarrade <fabien.tarrade@gmail.com>
2020-06-17 17:54:10 +0200

    adding more complux ci/cd pipelines

--- docker/test/check_env.sh
+++ docker/test/check_env.sh
@@ -2,6 +2,10 @@
 echo "Checking input env variable:"
 echo "--> PROJECT"
 echo $PROJECT
+echo "--> IMAGE CPU"
+echo $IMAGE_CPU
+echo "--> IMAGE GPU"
+echo $IMAGE_GPU
 echo "--> TAG"
 echo $TAG
 echo "--> DOCKERFILE"
